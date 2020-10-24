5189bf274f8d5206b7671a282cf57d74333efee3
benmyrat <benmyrat@gmail.com>
2020-10-24 12:54:33 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle('Главная');
+?>
+<h1>I love bitrix</h1>
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
